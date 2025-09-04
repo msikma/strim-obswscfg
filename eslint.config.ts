d17ebd228814a63fd913013bcfa1b642cbd019fb
Michiel Sikma <michiel@sikma.org>
2025-09-04 16:21:31 +0200

    Switch to prepared eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,33 +1,3 @@
-import stylistic from '@stylistic/eslint-plugin'
-import eslint from '@eslint/js'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
+import {nodeConfig} from '@dada78641/eslint-config'
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    plugins: {
-      '@stylistic': stylistic
-    }
-  },
-  {
-    rules: {
-      '@stylistic/quotes': ['error', 'single', {allowTemplateLiterals: 'always', avoidEscape: true}],
-      '@stylistic/no-tabs': ['error'],
-      '@stylistic/object-curly-spacing': ['error', 'never'],
-      '@stylistic/semi': ['error', 'never']
-    }
-  },
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node
-      }
-    }
-  },
-  {
-    ignores: ['dist/']
-  }
-)
+export default nodeConfig
